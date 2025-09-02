7eb32a560dd81831f3bbbbc0a21839b137de1040
Rémi DEHER <remi.deher@deher.net>
2025-09-02 11:44:02 +0200

    reoutes config

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Events\CashCountUpdated;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
+Route::post('/counts', function (Request $request) {
+    $data = $request->validate(['total' => 'required|numeric']);
+    broadcast(new CashCountUpdated($data))->toOthers();
+
+    return response()->json(['status' => 'success', 'data' => $data]);
 })->middleware('auth:sanctum');
